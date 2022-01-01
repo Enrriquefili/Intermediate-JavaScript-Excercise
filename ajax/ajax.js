0df2be97d902c672409a526062506e1baac8eaf8
Carlos <carlosjuniorrvega@hotmail.com>
2021-12-31 19:01:41 -0500

    esta es la primera solucion

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,4 +1,6 @@
 let arr=[true,false,true,false,true,true,true];
+// Existen varias formas de contar los true de este objeto iterable.Se puede hacer con "for of" ,con el "for" loop, se puede usar "forEach", con un "map", pero de momento te mando esta solucion
+
 
 function countTrue(iterable){
     let count =0;
